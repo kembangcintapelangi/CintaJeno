957c3a94e5ea3caeda39ac34687d981b0e4de7e9
kembangcintapelangi <cintakembangpelangi@gmail.com>
2026-05-11 12:02:42 +0700

    revisian tugas 3

--- t_kat.php
+++ t_kat.php
@@ -258,14 +258,14 @@ if (isset($_POST['simpan'])) {
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>CintaJeno</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
       <!-- All the links in the footer should remain intact. -->
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      Designed by <a href="https://www.instagram.com/floverain_">Cinta</a>
     </div>
   </footer><!-- End Footer -->
 
